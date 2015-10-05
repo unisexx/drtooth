a2f94796fc378bce85f9e3e9134178715aff2a93
unisexx <unisexx@gmail.com>
2015-10-06 02:14:14 +0700

    first input

---
+++ application/modules/patients/views/admin/form.php
@@ -0,0 +1,113 @@
+<div class="breadcrumbs" id="breadcrumbs">
+	<ul class="breadcrumb">
+		<li>
+			<i class="icon-home home-icon"></i>
+			<a href="#">หน้าแรก</a>
+
+			<span class="divider">
+				<i class="icon-angle-right arrow-icon"></i>
+			</span>
+		</li>
+		<li class="active">ฟอร์ม</li>
+	</ul><!--.breadcrumb-->
+
+	<!-- <div class="nav-search" id="nav-search">
+		<form class="form-search" />
+			<span class="input-icon">
+				<input type="text" placeholder="Search ..." class="input-small nav-search-input" id="nav-search-input" autocomplete="off" />
+				<i class="icon-search nav-search-icon"></i>
+			</span>
+		</form>
+	</div> -->
+	<!--#nav-search-->
+</div>
+
+<div class="page-content">
+	<div class="page-header position-relative">
+		<h1>
+			คนไข้ของเรา
+			<!-- <small>
+				<i class="icon-double-angle-right"></i>
+				<?=@$_GET['category']?>
+			</small> -->
+		</h1>
+	</div><!--/.page-header-->
+
+	<div class="row-fluid">
+		<div class="span12">
+			<!--PAGE CONTENT BEGINS-->
+				<form class="form-horizontal" method="post" action="patients/admin/patients/save/<?php echo $rs->id?>" enctype="multipart/form-data">
+					
+					<div class="control-group">
+			            <label class="control-label" for="id-input-file-1">รูปภาพ</label>
+			            <div class="controls">
+			                <?php if($rs->image):?>
+			                <img class="img" style="width:100px;" src="<?php echo (is_file('uploads/patients/'.$rs->image))? 'uploads/patients/'.$rs->image : 'media/images/webboard/noavatar.gif' ?>"  /> <br><br>
+			                <?php endif;?>
+			                <div class="input-xxlarge">
+			                    <input type="file" id="id-input-file-1" name="image"/>
+			                </div>
+			            </div>
+			        </div>
+					
+					<!-- select box -->
+			        <!-- <div class="control-group">
+			            <label class="control-label" for="username">กลุ่ม</label>
+			            <div class="controls">
+			                <?php echo form_dropdown('user_type_id',get_option('id','name','user_types','order by id asc'),$user->user_type_id,'');?>
+			            </div>
+			        </div> -->
+					
+					<div class="control-group">
+					    <label class="control-label" for="name">ชื่อ - นามสกุล</label>
+					    <div class="controls">
+					      <input id="name" class="span5" type="text" name="name" value="<?php echo $rs->name?>">
+					    </div>
+					</div>
+					
+					<div class="control-group">
+					    <label class="control-label" for="email">รายละเอียด</label>
+					    <div class="controls">
+					    	<textarea name="detail" class="form-control span5" rows="3"><?=$rs->detail?></textarea>
+					    </div>
+					</div>
+					
+					<div class="form-actions">
+						<?php echo @form_referer() ?>
+						<button class="btn btn-large btn-info" type="submit">
+							<i class="icon-ok bigger-110"></i>บันทึก
+						</button>
+
+						&nbsp; &nbsp; &nbsp;
+						<button class="btn btn-large" type="reset">
+							<i class="icon-undo bigger-110"></i>รีเซ็ต
+						</button>
+					</div>
+				
+				</form>
+				
+			<!--PAGE CONTENT ENDS-->
+		</div><!--/.span-->
+	</div><!--/.row-fluid-->
+</div>
+
+<!-- Load TinyMCE -->
+<script type="text/javascript" src="media/tiny_mce/tiny_mce.js"></script>
+<script type="text/javascript" src="media/tiny_mce/config.js"></script>
+<script type="text/javascript">
+// tiny('detail');
+$(function() {
+	$('#id-input-file-1 , #id-input-file-2').ace_file_input({
+		no_file:'ไม่มีไฟล์แนบ...',
+		btn_choose:'แนบไฟล์',
+		btn_change:'เปลี่ยน',
+		droppable:false,
+		onchange:null,
+		thumbnail:false //| true | large
+		//whitelist:'gif|png|jpg|jpeg'
+		//blacklist:'exe|php'
+		//onchange:''
+		//
+	});
+});
+</script>
---
+++ themes/ace_admin/_menu.php
@@ -0,0 +1,187 @@
+
+
+<div class="sidebar" id="sidebar">
+	<!-- <div class="sidebar-shortcuts" id="sidebar-shortcuts">
+		<div class="sidebar-shortcuts-large" id="sidebar-shortcuts-large">
+			<button class="btn btn-small btn-success">
+				<i class="icon-signal"></i>
+			</button>
+
+			<button class="btn btn-small btn-info">
+				<i class="icon-pencil"></i>
+			</button>
+
+			<button class="btn btn-small btn-warning">
+				<i class="icon-group"></i>
+			</button>
+
+			<button class="btn btn-small btn-danger">
+				<i class="icon-cogs"></i>
+			</button>
+		</div>
+
+		<div class="sidebar-shortcuts-mini" id="sidebar-shortcuts-mini">
+			<span class="btn btn-success"></span>
+
+			<span class="btn btn-info"></span>
+
+			<span class="btn btn-warning"></span>
+
+			<span class="btn btn-danger"></span>
+		</div>
+	</div> -->
+	<!--#sidebar-shortcuts-->
+
+<ul class="nav nav-list">
+    
+<li <?=@$this->uri->segment(1) == 'hilights'?'class="active"':'';?>>
+	<a href="hilights/admin/hilights">
+		<i class="fa fa-file-image-o"></i>
+		<span class="menu-text"> ไฮไลท์ </span>
+	</a>
+</li>
+		
+
+<li <?=@$_GET['module'] == 'เกี่ยวกับเรา'?'class="active open"':'';?>>
+	<a href="#" class="dropdown-toggle">
+		<i class="fa fa-file-pdf-o"></i>
+		<span class="menu-text"> เกี่ยวกับเรา </span>
+
+		<b class="arrow icon-angle-down"></b>
+	</a>
+
+	<ul class="submenu">
+		<li <?=@$_GET['module'] == 'เกี่ยวกับเรา' && @$_GET['category'] == 'บริการของเรา'?'class="active open"':'';?>>
+			<a href="contents/admin/contents/form?module=เกี่ยวกับเรา&category=บริการของเรา">
+				<i class="icon-double-angle-right"></i>
+				บริการของเรา
+			</a>
+		</li>
+
+		<li <?=@$_GET['module'] == 'เกี่ยวกับเรา' && @$_GET['category'] == 'เหตุผลที่คุณควรเลือกเรา'?'class="active open"':'';?>>
+			<a href="contents/admin/contents/form?module=เกี่ยวกับเรา&category=เหตุผลที่คุณควรเลือกเรา">
+				<i class="icon-double-angle-right"></i>
+				เหตุผลที่คุณควรเลือกเรา
+			</a>
+		</li>
+		
+		<li <?=@$_GET['module'] == 'เกี่ยวกับเรา' && @$_GET['category'] == 'เครื่องมือของเรา'?'class="active open"':'';?>>
+			<a href="contents/admin/contents/form?module=เกี่ยวกับเรา&category=เครื่องมือของเรา">
+				<i class="icon-double-angle-right"></i>
+				เครื่องมือของเรา
+			</a>
+		</li>
+	</ul>
+</li>
+
+
+<li <?=@$_GET['module'] == 'บริการทางทันตกรรม'?'class="active open"':'';?>>
+	<a href="#" class="dropdown-toggle">
+		<i class="fa fa-file-pdf-o"></i>
+		<span class="menu-text"> บริการทางทันตกรรม </span>
+
+		<b class="arrow icon-angle-down"></b>
+	</a>
+
+	<ul class="submenu">
+		<li <?=@$_GET['module'] == 'บริการทางทันตกรรม' && @$_GET['category'] == 'บริการทั้งหมด'?'class="active open"':'';?>>
+			<a href="contents/admin/contents/form?module=บริการทางทันตกรรม&category=บริการทั้งหมด">
+				<i class="icon-double-angle-right"></i>
+				บริการทั้งหมด
+			</a>
+		</li>
+
+		<li <?=@$_GET['module'] == 'บริการทางทันตกรรม' && @$_GET['category'] == 'ทันตกรรมทั่วไป'?'class="active open"':'';?>>
+			<a href="contents/admin/contents/form?module=บริการทางทันตกรรม&category=ค่าการศึกษาบุตร">
+				<i class="icon-double-angle-right"></i>
+				ทันตกรรมทั่วไป
+			</a>
+		</li>
+		
+		<li <?=@$_GET['module'] == 'บริการทางทันตกรรม' && @$_GET['category'] == 'ทันตกรรมจัดฟัน'?'class="active open"':'';?>>
+			<a href="contents/admin/contents/form?module=บริการทางทันตกรรม&category=ทันตกรรมจัดฟัน">
+				<i class="icon-double-angle-right"></i>
+				ทันตกรรมจัดฟัน
+			</a>
+		</li>
+		
+		<li <?=@$_GET['module'] == 'บริการทางทันตกรรม' && @$_GET['category'] == 'ทันตกรรมจัดฟัน'?'class="active open"':'';?>>
+			<a href="contents/admin/contents/form?module=บริการทางทันตกรรม&category=ทันตกรรมเพื่อความงาม">
+				<i class="icon-double-angle-right"></i>
+				ทันตกรรมเพื่อความงาม
+			</a>
+		</li>
+		
+		<li <?=@$_GET['module'] == 'บริการทางทันตกรรม' && @$_GET['category'] == 'ทันตกรรมจัดฟัน'?'class="active open"':'';?>>
+			<a href="contents/admin/contents/form?module=บริการทางทันตกรรม&category=ทันตกรรมประดิษฐ์และรากเทียม">
+				<i class="icon-double-angle-right"></i>
+				ทันตกรรมประดิษฐ์และรากเทียม
+			</a>
+		</li>
+		
+		<li <?=@$_GET['module'] == 'บริการทางทันตกรรม' && @$_GET['category'] == 'ทันตกรรมจัดฟัน'?'class="active open"':'';?>>
+			<a href="contents/admin/contents/form?module=บริการทางทันตกรรม&category=ศัลยกรรมช่องปากและโรคเหงือก">
+				<i class="icon-double-angle-right"></i>
+				ศัลยกรรมช่องปากและโรคเหงือก
+			</a>
+		</li>
+	</ul>
+</li>
+
+
+<li <?=@$_GET['module'] == 'คุยกับหมอฟัน'?'class="active open"':'';?>>
+	<a href="#" class="dropdown-toggle">
+		<i class="fa fa-file-pdf-o"></i>
+		<span class="menu-text"> คุยกับหมอฟัน </span>
+
+		<b class="arrow icon-angle-down"></b>
+	</a>
+
+	<ul class="submenu">
+		<li <?=@$_GET['module'] == 'คุยกับหมอฟัน' && @$_GET['category'] == 'บริการทั้งหมด'?'class="active open"':'';?>>
+			<a href="contents/admin/contents/form?module=คุยกับหมอฟัน&category=ทีมทันตแพทย์ของเรา">
+				<i class="icon-double-angle-right"></i>
+				ทีมทันตแพทย์ของเรา
+			</a>
+		</li>
+
+		<li <?=@$_GET['module'] == 'คุยกับหมอฟัน' && @$_GET['category'] == 'ทันตกรรมทั่วไป'?'class="active open"':'';?>>
+			<a href="contents/admin/contents/form?module=คุยกับหมอฟัน&category=คุยกับหมอฟัน">
+				<i class="icon-double-angle-right"></i>
+				คุยกับหมอฟัน
+			</a>
+		</li>
+		
+		<li <?=@$_GET['module'] == 'คุยกับหมอฟัน' && @$_GET['category'] == 'ทันตกรรมจัดฟัน'?'class="active open"':'';?>>
+			<a href="contents/admin/contents/form?module=คุยกับหมอฟัน&category=ทีมงานของเรา">
+				<i class="icon-double-angle-right"></i>
+				ทีมงานของเรา
+			</a>
+		</li>
+	</ul>
+</li>
+
+
+<li <?=@$this->uri->segment(1) == 'user_permission'?'class="active"':'';?>>
+	<a href="patients/admin/patients">
+		<i class="fa fa-globe"></i>
+		<span class="menu-text"> คนไข้ของเรา </span>
+	</a>
+</li>
+
+
+<li <?=@$this->uri->segment(1) == 'user_permission'?'class="active"':'';?>>
+	<a href="users/admin/users">
+		<i class="fa fa-globe"></i>
+		<span class="menu-text"> ติดต่อเรา </span>
+	</a>
+</li>
+		
+        
+		
+	</ul><!--/.nav-list-->
+
+	<div class="sidebar-collapse" id="sidebar-collapse">
+		<i class="icon-double-angle-left"></i>
+	</div>
+</div>
